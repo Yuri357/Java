b08996354dba641e1004052aa1fc30319a415d8b
Yuri357 <127751100+Yuri357@users.noreply.github.com>
2023-03-14 10:59:37 -0300

    Add files via upload

---
+++ exercito/src/exercito/exercito.java
@@ -0,0 +1,33 @@
+package exercito;
+
+import java.util.Scanner;
+
+public class exercito {
+
+	public static void main(String[] args) {
+	
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		System.out.println("Informe sua idade: ");
+		int idade = entrada.nextInt();
+		if (idade >= 18) {
+			System.out.println("Yuri vai para o Exercito!! ");
+		
+		} else {
+			System.out.println("Yuri não vai para o Exercito!!! ");
+			
+				
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
