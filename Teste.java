0f18a20e4497d8ca9c0382bff7539508d0884afb
Yuri357 <127751100+Yuri357@users.noreply.github.com>
2023-03-17 11:28:57 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println(" Informe um valor ");
+		int n1 = entrada.nextInt();
+		
+		if (n1 % 2 == 0) {
+			
+			System.out.println(n1 + " é par ");
+		}else {
+			System.out.println(n1 + " é impar ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
