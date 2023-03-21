544c9b41c753f84afc93c3c9589cc4b698d2698f
Yuri357 <127751100+Yuri357@users.noreply.github.com>
2023-03-21 09:50:26 -0300

    Add files via upload

---
+++ Por3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Por3 {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println(" Informe um Número: ");
+		int n1 = entrada.nextInt();
+		
+		if (n1 % 3 == 0) {
+			System.out.println(" É multiplo de 3 !!!! ");
+			
+			
+		} else { 
+			
+			System.out.println(" Não é multiplo de 3 !!!! ");
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
